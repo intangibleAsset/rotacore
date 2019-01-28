3e81c96badf3d45c15f5bedb836992fd5e697246
intangibleAsset <pingsucceeded@googlemail.com>
2019-01-28 23:35:52 +0000

    added section strings

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('shift/', views.shift, name="shift"),
     path('', views.index, name="index"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def shift(request):
+    sec = request.GET.get("section","error")#returns section or 1
+    
+
+
+
+
+    context = {'result':sec}
+    return render(request, 'core/shift.html',context)
